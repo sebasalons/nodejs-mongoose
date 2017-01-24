117b9b0f47b19ef16f942e26878cf9ae3a952f0f
Raquel Sánchez <raquel.sanchez@atrapalo.com>
2017-01-25 00:43:19 +0100

    Implemented setting configuration

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ require('module').Module._initPaths();
 const app = require('express')();
 const apiRead = require('src/apiRead');
 const apiCreate = require('src/apiCreate');
-const version = '0.1',
-      port = 3001;
+const settings = require('src/Config/settings');
 
 //Router configuration.
-app.use('/api/' + version, apiRead);
-app.use('/api/' + version, apiCreate);
-app.listen(port, function () {
-    console.log(`[Booking Persistable Service] running in port ${port}`);
+app.use('/api/' + settings.connection.version, apiRead);
+app.use('/api/' + settings.connection.version, apiCreate);
+app.listen(settings.connection.port, function () {
+    console.log(`[Booking Persistable Service] running in port ${settings.connection.port}`);
 });
---
+++ src/Config/settings.js
@@ -0,0 +1,10 @@
+module.exports = {
+    connection: {
+        port: 3001,
+        version: 0.1
+    },
+    mongodb: {
+        url: 'mongodb://localhost/',
+        database: 'library'
+    }
+};
