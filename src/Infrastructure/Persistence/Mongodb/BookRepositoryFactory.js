f1fbe32f2de85e1b979ac28159b4e3d3b966d75f
Bernat Segura <bernat.st@gmail.com>
2017-02-16 19:33:05 +0100

    Removed unnecessary code, fixed error (lack of new) and improved documentation

--- src/Infrastructure/Persistence/Mongodb/BookRepositoryFactory.js
+++ src/Infrastructure/Persistence/Mongodb/BookRepositoryFactory.js
@@ -1,28 +1,13 @@
-const BookModel = require('src/Infrastructure/Persistence/Mongodb/Model/BookModel');
 const Book = require('src/Domain/Book');
 
 /**
- * @param array data
- * @returns BookModel
- */
-module.exports.transformArrayToModel = function(data)
-{
-    return BookModel({
-        id: data['id'],
-        name: data['name'],
-        author: data['author'],
-        pages: data['pages'],
-        publisher: data['publisher']
-    });
-};
-
-/**
- * @param Object data
+ * @param data
+ *
  * @returns Book
  */
 module.exports.transformObjectToBook = function(data)
 {
-    return Book(
+    return new Book(
         data.id,
         data.name,
         data.author,
